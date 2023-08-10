1e53a661205bda170af77bd044eb82b3854c5b5b
Diego Arquieta <arquieta11@gmail.com>
2023-08-09 18:39:15 -0600

    Config mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPagetitle: 'custom-title'
+  },
   e2e: {
     baseUrl: "https://www.liverpool.com.mx",
     "chromeWebSecurity": false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}"
   },
