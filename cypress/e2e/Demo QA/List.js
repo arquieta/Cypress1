1bbae08aa470d626a956516307beab1c3474b967
Diego Arquieta <arquieta11@gmail.com>
2023-08-10 16:55:49 -0600

    Trying different methods

--- cypress/e2e/Demo QA/List.js
+++ cypress/e2e/Demo QA/List.js
@@ -16,7 +16,7 @@ describe('Validate diferent methods to use lists', () => {
 		cy.wait(10000)
 		
 		})
-        it.only('Use within for the list', () => {
+        it('Use within for the list', () => {
 		
             cy.get('.category-cards .card-body').contains('Interactions').click()
     
@@ -30,6 +30,36 @@ describe('Validate diferent methods to use lists', () => {
             cy.wait(10000)
             
             })
+
+            it.only('Use within for the list', () => {
+		
+                cy.get('.category-cards .card-body').contains('Interactions').click()
+        
+                cy.get('.left-pannel .accordion .element-group .element-list.collapse.show .menu-list').within(() => {
+                    // .should('be.visible')
+                    cy.contains('Selectable')
+                    .click()
+                })
+                cy.url().should('include', '/selectable')
+                cy.get('ul#verticalListContainer li').should('be.visible')
+
+                cy.get('#verticalListContainer li').each(($li) => {
+                    const expectedText = $li.text();
+                    
+                    // Click on the option
+                    $li.click();
+                    
+                    // Validate the correct text is being displayed
+                    cy.get('.mt-2.list-group-item.active.list-group-item-action').should('have.text', expectedText);
+                });
+                // cy.get('ul#verticalListContainer').within(() => {
+                //     cy.get('.mt-2.list-group-item.list-group-item-action').should('be.visible')
+                //     cy.get('li:first-child .mt-2.list-group-item.list-group-item-action').should('be.visible')
+                // })
+                   // cy.get('.header-text').contains('Interactions').click()
+                cy.wait(10000)
+                
+                })
 
 	})
 	
