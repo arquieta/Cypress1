9c38b645de77431d100eeb39e89710b69429fb6d
arquieta <arquieta11@gmail.com>
2024-11-27 17:59:26 -0600

    deletion of files not needed

--- cypress/e2e/reverseNumber.js
+++
@@ -1,9 +0,0 @@
-function reverseNum(num){
-    let reverString = num.toString().split("").reverse().join("");
-
-    let reversNum = parseInt(reverString);
-
-    return reversNum;
-
-}
-console.log(reversNum(12345));
